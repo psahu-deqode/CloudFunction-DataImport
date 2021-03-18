8d5c0c263bd15d70af92240229d9c50126a046f1
Puja Sahu <psahu@deqode.com>
2021-03-18 19:10:52 +0530

    cloud function for posting file name in cloud bucket and copying json data to datastore

---
+++ main.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import make_response
+import os
+import json
+from google.cloud import storage, datastore
+
+PROJECT_ID = os.getenv('PROJECT_ID')
+SERVICE_ACCOUNT_JSON = os.getenv('SERVICE_ACCOUNT_JSON')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+app = Flask(__name__)
+
+
+@app.route('/main/', methods=['POST'])
+def main(request):
+    req = request.get_json(silent=True, force=True)
+    res = makeResponse(req)
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def makeResponse(req):
+    # create storage client
+    storage_client = storage.Client.from_service_account_json(SERVICE_ACCOUNT_JSON)
+    # get bucket with name
+    bucket = storage_client.get_bucket(BUCKET_NAME)
+    json_file = req.get("filename")
+    # get bucket data as blob
+    blob = bucket.get_blob(json_file)
+    data = json.loads(blob.download_as_string())
+    for i in data['ContentsList']:
+        # append details to each json in ContentsList
+        i['DocumentNo'] = data['DocumentNo']
+        i['LanguageCode'] = data['LanguageCode']
+        i['Version'] = data['Version']
+        # Converted comma separated unit names to Array so it will be easier to search
+        i['UnitName'] = i['UnitName'].split(', ') if len(i['UnitName']) != 0 else i['UnitName']
+        # create datastore entity
+        imported_json = datastore.Entity(key=datastore.Client(PROJECT_ID).key("data"))
+        # update datastore entity with the imported json
+        imported_json.update(i)
+        datastore.Client(PROJECT_ID).put(imported_json)
+    return "Json uploaded successfully to Datastore"
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
